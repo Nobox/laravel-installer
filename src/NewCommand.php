33a60a10c8845be8ffa1effdbe0afd29d029c1ea
Alberto Estrada Puerta <albertoe@gmail.com>
2015-12-10 10:11:54 -0500

    Divided processes and extracted some methods intro traits

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -12,11 +12,19 @@ use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Process\Process;
 use ZipArchive;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Nobox\Traits\Files;
+use Nobox\Traits\Environment;
+use Nobox\Traits\Github;
 
 class NewCommand extends Command
 {
     private $client;
     protected $gitHubStatus;
+    protected $projectName;
+    protected $linkedGithubAccount;
+    protected $githubConfig;
+
+    use Files, Environment, Github;
 
     public function __construct(ClientInterface $client)
     {
@@ -35,44 +43,14 @@ class NewCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $projectName = $input->getArgument('name');
+        $this->projectName = $input->getArgument('name');
 
-        $helper = $this->getHelper('question');
-
-        $directory = getcwd() . '/' . $projectName;
+        $directory = getcwd() . '/' . $this->projectName;
 
         // assert that the forlder doesn't already exist
         $this->assertApplicationDoesNotExist($directory, $output);
 
-        // Ask the user for github repo (optional)
-        $question = new ConfirmationQuestion(
-            'Do you want to link an empty github repository to this project? (y/n): ',
-            false,
-            '/^(y|j)/i'
-        );
-
-        $hasGithub = $helper->ask($input, $output, $question);
-
-
-        if ($hasGithub) {
-
-
-            $this->checkGithubAccount('git config --global user.email', $directory, $output);
-
-
-            if (!$this->gitHubStatus) {
-                $emailQuestion = new Question('<question>What is your github email?</question> ', $projectName);
-                $githubEmail = $helper->ask($input, $output, $emailQuestion);
-                $gitConfig['email'] = $githubEmail;
-            }
-
-
-            // Ask the user for the repository url
-            $question = new Question('<question>What is the repository url (git@github.com:repo.git) ?</question> ', $projectName);
-            $repositoryUrl = $helper->ask($input, $output, $question);
-
-            $gitConfig['url'] = $repositoryUrl;
-        }
+        $this->githubConfig = $this->askForGithub($input, $output, $directory);
 
 
         $output->writeln('<comment>Crafting application...</comment>');
@@ -87,9 +65,9 @@ class NewCommand extends Command
 
 
 
-        if ($hasGithub) {
+        if (is_array($this->githubConfig)) {
             // Proceed to github setup
-            $this->setupGitProject($directory, $output, $gitConfig);
+            $this->setupGitProject($directory, $output, $this->githubConfig);
 
         }
 
@@ -106,90 +84,64 @@ class NewCommand extends Command
     }
 
     /**
-     * Check if the application folder exists
-     * @param  [path]          $directory [description]
-     * @param  OutputInterface $output    [description]
-     * @return [void]
-     */
-    private function assertApplicationDoesNotExist($directory, OutputInterface $output)
-    {
-        if (is_dir($directory)) {
-            $output->writeln('<error>Application already exists!</error>');
-
-            exit(1);
-        }
-    }
-
-    /**
      * Download repository zip file
      * @param  [path] $zipFile Destination path
      */
     private function download($zipFile, $output)
     {
-        $output->writeln('<comment>Downloading repository...</comment>');
+        $output->write('<comment>Downloading repository...</comment> ');
 
         $response = $this->client->get('https://github.com/Nobox/laravel/archive/master.zip')->getBody();
 
         file_put_contents($zipFile, $response);
+        $output->write('<info>√ done</info>', 1);
 
         return $this;
     }
-
-    private function extract($zipFile, $directory, $output)
-    {
-        $output->writeln('<comment>Extracting repository...</comment>');
-
-        $archive = new ZipArchive;
-
-        $currentDir = getcwd();
-
-        $archive->open($zipFile);
-        $archive->extractTo($currentDir);
-        $archive->close();
-
-        return $this;
-    }
-
-    private function cleanUp($zipFile)
-    {
-        @chmod($zipFile, 0777);
-        @unlink($zipFile);
-
-        return $this;
-    }
-
-    private function rename($directory)
-    {
-        $currentExtracedDir = getcwd() . '/laravel-master';
-        rename($currentExtracedDir, $directory);
-
-        return $this;
-    }
-
 
     private function install($directory, OutputInterface $output)
     {
-        $output->writeln('<info>Installing dependencies from composer, npm and bower.</info>');
+        $output->writeln('');
+        $output->writeln('<comment>Beginning dependencies installation...</comment>');
 
         $composer = $this->findComposer();
 
-        $commands = [
-            $composer.' install',
-            'npm install',
-            'bower install',
-            'cp .env.example .env',
-            'php artisan key:generate',
-            'gulp'
+        $batch1 = [
+            'composer' => [
+                'title' => 'Running composer dependencies installation',
+                'line' => $composer.' install',
+                'progress' => true
+            ],
+            'npm' => [
+                'title' => 'Running npm dependencies installation (this will take some time...)',
+                'line' => 'npm install',
+                'progress' => false,
+            ],
+            'bower' => [
+                'title' => 'Runnning bower dependencies installation',
+                'line' => 'bower install',
+                'progress' => true,
+            ]
+        ];
+        // install batch
+        $this->processIterator($batch1, $directory, $output);
+
+        $batch2 = [
+            'post-installation' => [
+                'title' => 'Running post installation commands',
+                'line' => [
+                    'cp .env.example .env',
+                    'php artisan key:generate',
+                    'gulp'
+                ],
+                'progress' => false
+            ]
         ];
 
-        $progress = new ProgressBar($output, 800);
-        $progress->setFormat('[%bar%] %percent%%');
-        $progress->start();
+        //post install batch
+        $this->processIterator($batch2, $directory, $output);
 
-        $this->runProcess($commands, $directory, $output, $progress);
 
-        $progress->finish();
-        $progress->clear();
 
         $output->writeln('');
         $output->writeln('<info>Dependencies installation completed.</info>');
@@ -198,89 +150,58 @@ class NewCommand extends Command
     }
 
 
-    private function setupGitProject($directory, $output, $config)
+    private function processIterator($commands, $directory, OutputInterface $output)
     {
-        $output->writeln('<info>Linking github repository to project.</info>');
 
-        if (!$this->gitHubStatus) {
-            $commands = [
-                'cd ' . $directory,
-                'git config --global user.email "' . $config['email'] . '"',
-                'git init',
-                'git add .',
-                'git commit -m "Project Setup"',
-                'git remote add origin ' . $config['url'],
-                'git push -u origin master'
-            ];
-        } else {
-            $commands = [
-                'cd ' . $directory,
-                'git init',
-                'git add .',
-                'git commit -m "Project Setup"',
-                'git remote add origin ' . $config['url'],
-                'git push -u origin master'
-            ];
+        foreach ($commands as $alias => $command) {
+            $this->runProcess($command, $directory, $output, $alias);
         }
-
-        $progress = new ProgressBar($output, 4);
-        $progress->setFormat('[%bar%] %percent%%');
-        $progress->start();
-
-        $this->runProcess($commands, $directory, $output, $progress);
-
-        $progress->finish();
-        $progress->clear();
-        $output->writeln('');
-
-        $output->writeln('<info>Git setup completed.</info>');
     }
-
-
-    private function checkGithubAccount($command, $directory, OutputInterface $output)
-    {
-        $process = new Process($command, $directory);
-
-        $process->run(function ($type, $line) use ($output) {
-            if ($line !== '') {
-                $this->gitHubStatus = true;
-            }
-        });
-    }
-
 
     /**
-     * Run a list of terminal commands
-     * @param  [array]         $commands  [description]
+     * Run a terminal command
+     * @param  [array]         $command  [description]
      * @param  [path]          $directory [description]
      * @param  OutputInterface $output    [description]
      * @return [void]                     [description]
      */
-    private function runProcess($commands, $directory, OutputInterface $output, $progress)
+    private function runProcess($command, $directory, OutputInterface $output, $alias)
     {
-        $process = new Process(implode(' && ', $commands), $directory, null, null, null);
+        $output->writeln('');
 
-        $process->run(function ($type, $line) use ($output, $progress) {
-            if ($output->isVerbose()) {
-                $progress->setFormat('%message% [%bar%] %percent%%');
-                $progress->setMessage($line);
+        if(is_array($command['line'])) {
+            $commandLine = implode(' && ', $command['line']);
+        } else {
+            $commandLine = $command['line'];
+        }
+
+
+        $process = new Process($commandLine, $directory);
+        $process->setTimeout(7600);
+        $process->start();
+
+        if ($output->isVerbose()) {
+            $process->wait(function ($type, $buffer) {
+                echo $buffer;
+            });
+        } else {
+
+            $progress = new ProgressBar($output);
+            $progress->setFormat("<comment>%message%</comment> [%bar%]");
+            $progress->setMessage($command['title']);
+            $progress->start();
+            $progress->setRedrawFrequency(10000);
+
+            while ($process->isRunning()) {
+                 $progress->advance();
             }
 
-            $progress->advance();
-        });
-    }
-
-    /**
-     * Get the composer command for the environment.
-     *
-     * @return string
-     */
-    protected function findComposer()
-    {
-        if (file_exists(getcwd().'/composer.phar')) {
-            return '"'.PHP_BINARY.'" composer.phar';
+            $progress->finish();
+            $progress->clear();
         }
-        return 'composer';
+
+        $output->writeln('');
+        $output->write('<comment>' . $command['title'] . ' </comment><info>√ done</info>');
     }
 
 }
