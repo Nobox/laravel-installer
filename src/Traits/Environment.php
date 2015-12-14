d82d4b39741b102e1cd5939eb5d9a9f04ea86237
Alberto Estrada Puerta <albertoe@gmail.com>
2015-12-14 18:47:28 -0500

    Changed case of traits folder

---
+++ src/Traits/Environment.php
@@ -0,0 +1,21 @@
+<?php
+namespace Nobox\Traits;
+
+use ZipArchive;
+
+trait Environment
+{
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer()
+    {
+        if (file_exists(getcwd().'/composer.phar')) {
+            return '"'.PHP_BINARY.'" composer.phar';
+        }
+        return 'composer';
+    }
+}
